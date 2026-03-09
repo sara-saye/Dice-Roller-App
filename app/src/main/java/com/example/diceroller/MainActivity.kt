b9048950476e81f52538106f3b30834fd23f9f9e
sara_saye <darwishsara841@gamil.com>
2026-03-09 21:24:47 +0200

    XML Done

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val image : ImageView = findViewById(R.id.diceimage)
+
+
     }
 }
